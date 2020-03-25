57c21a6dd7c718d9ce5cb84c454776e0a6bfe752
Mike O'Connell <guitarmike1966@gmail.com>
2020-03-25 10:30:33 -0500

    Initial Commit

---
+++ GitTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitTest
+//
+//  Created by mikey on 3/25/20.
+//  Copyright © 2020 Sand Creek Software. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
